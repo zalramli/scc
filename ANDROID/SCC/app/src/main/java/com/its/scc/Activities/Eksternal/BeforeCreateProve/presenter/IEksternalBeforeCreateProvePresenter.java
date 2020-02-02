51dbadedc3b79f581e62cfa018891384a1ba615b
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-02-02 09:56:11 +0700

    proses sebelum sebelum pemesanan prove
    proses sebelum sebelum pemesanan prove

--- ANDROID/SCC/app/src/main/java/com/its/scc/Activities/Eksternal/BeforeCreateProve/presenter/IEksternalBeforeCreateProvePresenter.java
+++ ANDROID/SCC/app/src/main/java/com/its/scc/Activities/Eksternal/BeforeCreateProve/presenter/IEksternalBeforeCreateProvePresenter.java
@@ -1,5 +1,5 @@
 package com.its.scc.Activities.Eksternal.BeforeCreateProve.presenter;
 
 public interface IEksternalBeforeCreateProvePresenter {
-	void onSubmit(String id_eksternal, String id_internal, String id_materi_prove, String id_jadwal_prove, String deskripsi_materi, String tanggal_booking, String tanggal_prove, String kode_prove, String kata_sandi);
+	void onSubmit(String id_eksternal, String id_internal, String id_materi_prove, String id_jadwal_prove, String deskripsi_materi, String tanggal_prove);
 }
