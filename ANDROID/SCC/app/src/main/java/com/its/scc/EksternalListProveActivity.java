76f0b22443c349fdfb1f3ee26b46d4e9d59d14ba
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-02-02 19:30:28 +0700

    persiapan sebelum list prove
    persiapan sebelum list prove

---
+++ ANDROID/SCC/app/src/main/java/com/its/scc/EksternalListProveActivity.java
@@ -0,0 +1,14 @@
+package com.its.scc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EksternalListProveActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_eksternal_list_prove);
+	}
+}
