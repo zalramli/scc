30a9861971d27b03d50e23ee27478878b453a473
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-02-20 01:22:22 +0700

    item adapter  + models software
    item adapter  + models software

---
+++ ANDROID/SCC/app/src/main/java/com/its/scc/Models/Software.java
@@ -0,0 +1,21 @@
+package com.its.scc.Models;
+
+public class Software {
+	String id_softwate, nama;
+
+	public String getId_softwate() {
+		return id_softwate;
+	}
+
+	public void setId_softwate(String id_softwate) {
+		this.id_softwate = id_softwate;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+}
