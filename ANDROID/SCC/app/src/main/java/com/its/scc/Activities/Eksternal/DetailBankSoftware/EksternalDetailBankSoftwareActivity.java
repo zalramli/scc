4fd3298ff4573bcae2d3a1d03f437d08ffb65168
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-02-24 01:26:19 +0700

    bank software done
    bank software done

--- ANDROID/SCC/app/src/main/java/com/its/scc/Activities/Eksternal/DetailBankSoftware/EksternalDetailBankSoftwareActivity.java
+++ ANDROID/SCC/app/src/main/java/com/its/scc/Activities/Eksternal/DetailBankSoftware/EksternalDetailBankSoftwareActivity.java
@@ -13,7 +13,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
