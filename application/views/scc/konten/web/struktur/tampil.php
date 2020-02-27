f85babaeadadabc950154647ffe441bd1dee5a92
Rizal Ramli <github.little@gmail.com>
2020-02-27 22:49:42 +0700

    Fitur Bagan Kepengurusan SCC WEB

--- application/views/scc/konten/web/struktur/tampil.php
+++ application/views/scc/konten/web/struktur/tampil.php
@@ -1,11 +1,184 @@
-  <!--================Hero Banner SM Area Start =================-->
-  <section class="hero-banner-sm-404 magic-ball magic-ball-banner" id="parallax-1" data-anchor-target="#parallax-1"
+<section class="hero-banner-sm-struktur magic-ball magic-ball-banner" id="parallax-1" data-anchor-target="#parallax-1"
     data-300-top="background-position: 0px -80px" data-top-bottom="background-position: 0 100px">
     <div class="container">
       <div class="hero-banner-sm-content">
-        <h1>&nbsp</h1>
-        <p class="mb-5">&nbsp</p>
+      <h1>Struktur Organisasi</h1>
+        <p>Bagan struktur kepengurusan SCC-HIMASTA ITS 2019/2020</p>
       </div>
     </div>
   </section>
-  <h1 class="text-center mb-5 mt-5">Not Found</h1>
+
+  <section class="section-margin">
+    <div class="section-intro text-center pb-90px">
+        <h2>Bagan Struktur</h2>
+    </div>
+    <div class="container-fluid">
+      <div class="mb-5 pb-4">
+      <div class="content">
+  <figure class="org-chart cf">
+    <div class="board ">
+    <ul class="columnOne">
+        <li>
+          <span class="lvl-d">
+              <strong>Ketua Divisi</strong>
+              <br>Hasri Wiji Aqsari
+              </span>
+        </li>
+      </ul>
+      
+      <ul class="columnTwo">
+        <li>
+          <span class="lvl-c">
+              <strong>Sekretaris</strong>
+              <br>Tiza Ayu Virania
+              </span>
+        </li>
+        <li>
+          <span class="lvl-c">
+              <strong>Bendahara</strong>
+              <br>Seza Dwiwulan Ramadini
+              </span>
+        </li>
+      </ul>
+      <ul class="columnThree">
+        <li>
+          <span>
+               <strong>Manager HRD</strong>
+               <br>Maharani Sinta Nirwana
+               </span>
+        </li>
+        <li>
+          <span>
+               <strong>Manager PR</strong>
+               <br>Hardan Tri Laksamana
+               </span>
+        </li>
+        <li>
+          <span>
+               <strong>Manager TD</strong>
+               <br>Dhany Nastiti Lestari
+               </span>
+        </li>
+      </ul>
+    </div>
+    <ul class="departments ">
+      <li class="department ">
+        <span class="lvl-b">
+            <strong>Staff Ahli HRD</strong>
+            <br>Ratnanda Gita Alfitri
+            </span>
+        <ul class="sections">
+          <li class="section"> <span>
+                  <strong>Staff HRD</strong>
+                  <br>Vanda Fitriyanah
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff HRD</strong>
+                  <br>Lidya Cahya Aurellia
+                  </span>
+          </li>
+        </ul>
+      </li>
+      <li class="department ">
+        <span class="lvl-b">
+            <strong>Staff Ahli HRD</strong>
+            <br>Moch Akbar Pasha Perdana
+            </span>
+        <ul class="sections">
+          <li class="section"> <span>
+                  <strong>Staff HRD</strong>
+                  <br>Putri Hidayati Rohmah
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff HRD</strong>
+                  <br>Syafniya Zilfah Aniesiy
+                  </span>
+          </li>
+        </ul>
+      </li>
+      <li class="department ">
+        <span class="lvl-b">
+            <strong>Staff Ahli TD</strong>
+            <br>Muhammad Rayhan Lahdji
+            </span>
+        <ul class="sections">
+          <li class="section"> <span>
+                  <strong>Staff TD</strong>
+                  <br>Aubert Oktaviantono
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff TD</strong>
+                  <br>Yuniar Mega Kartikasari
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff TD</strong>
+                  <br>Nauli Mazaya Siregar
+                  </span>
+          </li>
+        </ul>
+      </li>
+      <li class="department">
+        <span class="lvl-b">
+            <strong>Staff Ahli TD</strong>
+            <br>Putri Wahyuni
+            </span>
+        <ul class="sections">
+          <li class="section"> <span>
+                  <strong>Staff TD</strong>
+                  <br>Eva Marella
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff TD</strong>
+                  <br>Sarnita Sadya
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff TD</strong>
+                  <br>Inka Nurul Alfiana
+                  </span>
+          </li>
+        </ul>
+      </li>
+      <li class="department ">
+        <span class="lvl-b">
+            <strong>Staff Ahli PR</strong>
+            <br>Diza Amalia
+            </span>
+        <ul class="sections">
+          <li class="section"> <span>
+                  <strong>Staff PR</strong>
+                  <br>Kevin Agung Fernanda Rifki
+                  </span>
+          </li>
+          <li class="section"> <span>
+                  <strong>Staff PR</strong>
+                  <br>Ichwanul Kahfi Prasetya
+                  </span>
+          </li>
+        </ul>
+      </li>
+      <li class="department ">
+        <span class="lvl-b">
+            <strong>Staff Ahli PR</strong>
+            <br>Muhammad Rofiqul Ilmi
+            </span>
+        <ul class="sections">
+          <li class="section"> <span>
+                  <strong>Staff PR</strong>
+                  <br>Shecilia Nur Salsalbila
+                  </span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </figure>
+</div>
+</div>
+    </div>
+  </section>
+  
